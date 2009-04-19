48b6c4a27a0ecd0a6284ace95191795cf7be6b6f
Jochen Keil <jochen.keil@gmail.com>
2009-04-19 22:07:03 +0200

    update main interface/api file

--- src/libcue/libcue.h
+++ src/libcue/libcue.h
@@ -5,5 +5,7 @@
  * For license terms, see the file COPYING in this distribution.
  */
 
-Cd *cue_parse(FILE *fp);
-void cue_print(FILE *fp, Cd *cd);
+#include "cd.h"
+
+Cd* cue_parse_file(FILE*);
+Cd* cue_parse_string(const char*);
