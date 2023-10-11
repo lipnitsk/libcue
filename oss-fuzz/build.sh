e6c5660c62f89fe0ae6f1413883b375768baa05a
Alex <aleksandrosansan@gmail.com>
2023-10-11 17:24:15 +0000

    Build fuzzer

---
+++ oss-fuzz/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+mkdir bin
+cd bin
+cmake -DCMAKE_BUILD_TYPE=Release ../
+make
+
+fuzzer_name=fuzz
+cp ${fuzzer_name} $OUT/${fuzzer_name}
+cp $SRC/libcue/oss-fuzz/cue.dict $OUT/${fuzzer_name}.dict
+
+find $SRC/libcue/t -iname "*.cue" | \
+     xargs zip $OUT/${fuzzer_name}_seed_corpus.zip
+
+echo -en "[libfuzzer]\nclose_fd_mask=2\n" >$OUT/${fuzzer_name}.options
