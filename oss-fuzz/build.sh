a40239e3821341f71794b64adf6376ff901b9546
Alex <aleksandrosansan@gmail.com>
2023-10-12 11:20:53 +0000

    Test corpus for regressions

--- oss-fuzz/build.sh
+++ oss-fuzz/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -eu
 
-mkdir bin
-cd bin
-cmake -DCMAKE_BUILD_TYPE=Release ../
+cd $WORK
+cmake -DCMAKE_BUILD_TYPE=Release $SRC/libcue
 make
 
 fuzzer_name=fuzz
@@ -10,6 +9,8 @@ cp ${fuzzer_name} $OUT/${fuzzer_name}
 cp $SRC/libcue/oss-fuzz/cue.dict $OUT/${fuzzer_name}.dict
 
 find $SRC/libcue/t -iname "*.cue" | \
+     xargs zip $OUT/${fuzzer_name}_seed_corpus.zip
+find $SRC/libcue/oss-fuzz/corpus -iname "*.cue" | \
      xargs zip $OUT/${fuzzer_name}_seed_corpus.zip
 
 echo -en "[libfuzzer]\nclose_fd_mask=2\n" >$OUT/${fuzzer_name}.options
