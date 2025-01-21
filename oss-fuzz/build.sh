aeae0b3dc435e906b28ca817e1ae70905ce67307
Daniel Löbl <dloebl.2000@gmail.com>
2025-01-21 21:17:28 +0100

    add new fuzz_full_coverage fuzzer

--- oss-fuzz/build.sh
+++ oss-fuzz/build.sh
@@ -4,13 +4,14 @@ cd $WORK
 cmake -DBUILD_FUZZER=ON -DCMAKE_BUILD_TYPE=Release $SRC/libcue
 make
 
-fuzzer_name=fuzz
-cp ${fuzzer_name} $OUT/${fuzzer_name}
-cp $SRC/libcue/oss-fuzz/cue.dict $OUT/${fuzzer_name}.dict
+for fuzzer_name in fuzz fuzz_full_coverage; do
+	cp ${fuzzer_name} $OUT/${fuzzer_name}
+	cp $SRC/libcue/oss-fuzz/cue.dict $OUT/${fuzzer_name}.dict
 
-find $SRC/libcue/t -iname "*.cue" | \
-     xargs zip $OUT/${fuzzer_name}_seed_corpus.zip
-find $SRC/libcue/oss-fuzz/corpus -iname "*.cue" | \
-     xargs zip $OUT/${fuzzer_name}_seed_corpus.zip
+	find $SRC/libcue/t -iname "*.cue" | \
+     		xargs zip $OUT/${fuzzer_name}_seed_corpus.zip
+	find $SRC/libcue/oss-fuzz/corpus -iname "*.cue" | \
+     		xargs zip $OUT/${fuzzer_name}_seed_corpus.zip
 
-echo -en "[libfuzzer]\nclose_fd_mask=2\n" >$OUT/${fuzzer_name}.options
+	echo -en "[libfuzzer]\nclose_fd_mask=2\n" >$OUT/${fuzzer_name}.options
+done
