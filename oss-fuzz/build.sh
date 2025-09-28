4babff8688835d4e96265b1e8eb6e621f989808c
Ilya Lipnitskiy <ilya.lipnitskiy@gmail.com>
2025-09-28 11:13:47 -0700

    Revert "add new fuzz_full_coverage fuzzer"
    This reverts commit aeae0b3dc435e906b28ca817e1ae70905ce67307.

--- oss-fuzz/build.sh
+++ oss-fuzz/build.sh
@@ -4,14 +4,13 @@ cd $WORK
 cmake -DBUILD_FUZZER=ON -DCMAKE_BUILD_TYPE=Release $SRC/libcue
 make
 
-for fuzzer_name in fuzz fuzz_full_coverage; do
-	cp ${fuzzer_name} $OUT/${fuzzer_name}
-	cp $SRC/libcue/oss-fuzz/cue.dict $OUT/${fuzzer_name}.dict
+fuzzer_name=fuzz
+cp ${fuzzer_name} $OUT/${fuzzer_name}
+cp $SRC/libcue/oss-fuzz/cue.dict $OUT/${fuzzer_name}.dict
 
-	find $SRC/libcue/t -iname "*.cue" | \
-     		xargs zip $OUT/${fuzzer_name}_seed_corpus.zip
-	find $SRC/libcue/oss-fuzz/corpus -iname "*.cue" | \
-     		xargs zip $OUT/${fuzzer_name}_seed_corpus.zip
+find $SRC/libcue/t -iname "*.cue" | \
+     xargs zip $OUT/${fuzzer_name}_seed_corpus.zip
+find $SRC/libcue/oss-fuzz/corpus -iname "*.cue" | \
+     xargs zip $OUT/${fuzzer_name}_seed_corpus.zip
 
-	echo -en "[libfuzzer]\nclose_fd_mask=2\n" >$OUT/${fuzzer_name}.options
-done
+echo -en "[libfuzzer]\nclose_fd_mask=2\n" >$OUT/${fuzzer_name}.options
